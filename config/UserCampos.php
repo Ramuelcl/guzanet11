665b3d29b2863e0c935610f03f576da6ef4fd5ff
Ramuelcl <43967974+Ramuelcl@users.noreply.github.com>
2024-07-11 18:51:39 +0200

    Update UserCampos.php

--- config/UserCampos.php
+++ config/UserCampos.php
@@ -39,7 +39,14 @@ return [
         'tipo' => 'mail',
  // restricciones de campo
 'rules' => 'required|email|unique:users,email,' . Auth::id(),
-
+// 'rules' => 'required|email|unique:users,email,' . Auth::id() . '|max:255|regex:/(.+)@(.+)\.[a-z]{2,}/',
+ 'messages' = [
+    'required' => 'El campo email es obligatorio.',
+    'email' => 'Introduce una dirección de correo electrónico válida.',
+    'unique' => 'El email ya está en uso.',
+    'max' => 'El email no puede superar los 255 caracteres.',
+    'regex' => 'El formato del email no es válido.',
+  ];
     'password' => [
         // lista
         'titulo1' => '',
@@ -50,8 +57,14 @@ return [
         // tipo de campo
         'tipo' => 'password',
  // restricciones de campo
-'crear'=>'password|required|min:1|max:100|unique:users',
-'editar'=>'password|required|min:1|max:100|unique:users',   ],
+'rules' = [
+    'password' => 'required|min:6|confirmed',
+    'password_confirmation' => 'required',
+// 'password' => 'required|min:8|regex:/[a-z]+[A-Z]+[0-9]+[@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/',
+$messages = [
+    'regex' => 'La contraseña debe contener al menos una minúscula, una mayúscula, un número y un carácter especial.',
+];
+];
 
     'profile_photo_path' => [
         // lista
